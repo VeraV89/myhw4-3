289261ce25a728f68219eb594b9106aab8635a9b
First test <vera6519@bk.ru>
2026-04-17 21:34:39 +0300

    steps

--- src/CreditPaymentService.java
+++ src/CreditPaymentService.java
@@ -1,7 +1,17 @@
 public class CreditPaymentService {
-    public int Calculate(double loanAmount, double loanTerm, double loanRate) {
-        double monthlyPayment = (loanAmount * (loanRate/12/100) / (1 - Math.pow(1 + (loanRate/12/100), -loanTerm)));
+    //    public int calculate(double loanAmount, double loanTerm, double loanRate) {
+//        double monthlyPayment = (loanAmount * (loanRate/12/100) / (1 - Math.pow(1 + (loanRate/12/100), -loanTerm)));
+//
+//
+//        return (int) monthlyPayment;
+//    }
+    public int calculate(double loanAmount, double loanTerm, double loanRate) {
+        double monthlyRate = loanRate / 12 / 100;
 
+        double numerator = loanAmount * monthlyRate;
+        double denominator = 1 - Math.pow(1 + monthlyRate, -loanTerm);
+
+        double monthlyPayment = numerator / denominator;
 
         return (int) monthlyPayment;
     }
---
+++ src/Main.java
@@ -0,0 +1,15 @@
+public class Main {
+    public static void main(String[] args) {
+        CreditPaymentService servise = new CreditPaymentService();
+
+        System.out.println();
+        System.out.println(servise.calculate(1_000_000, 12, 9.99));
+
+        System.out.println();
+        System.out.println(servise.calculate(1_000_000, 24, 9.99));
+
+        System.out.println();
+        System.out.println(servise.calculate(1_000_000, 36, 9.99));
+
+    }
+}
